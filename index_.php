b4b4b87657dd1baf97b203b8834a4fdfe6948dfa
at-voice <maaabel@yahoo.com>
2022-12-20 15:43:00 +0900

    投稿フォームの内容受け取りまで（SQL書き込みはまだ）

--- index_.php
+++ index_.php
@@ -28,6 +28,26 @@ $pdo = connect_to_db();
 <p>こんにちは＊＊さん</p>
 <a href="../php_senshuken/logout/logout.php">ログアウト</a>
 <a href="test.php">[test]ログアウトしてたら見られないページ</a>
+<p>相対パスをコピーって便利…</p>
+
+<div class="youtube_info_wrapper">
+  <div class="list_youtube">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/l-gF3W03mKE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+  <div class="list_comment">
+    <ul>
+      <li>すき</li>
+      <li>メロスは激怒した。必ず、かの邪智暴虐じゃちぼうぎゃくの王を除かなければならぬと決意した。メロスには政治がわからぬ。メロスは、村の牧人である。笛を吹き、羊と遊んで暮して来た。けれども邪悪に対しては、人一倍に敏感であった。きょう未明メロスは村を出発し、野を越え山越え、十里はなれた此このシラクスの市にやって来た。メロスには父も、母も無い。女房も無い。十六の、内気な妹と二人暮しだ。</li>
+      <li>ほにゃほにゃほにゃほにゃ</li>
+    </ul>
+  </div>
+
+</div>
+
+
+
+
+
 
 
 </body>
--- movie_info.php
+++ movie_info.php
@@ -20,6 +20,107 @@ $pdo = connect_to_db();
  </head>
 
 <body>
+<h2>
 ここはmovie_info.phpです
+</h2>
+
+<div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/l-gF3W03mKE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+      <div class="comment_list">
+        <ul>
+          <li>最後のシーンが好き</li>
+          <li>考えさせられる内容だった</li>
+          <li>記憶を消してもう一度見たい名作。ただし昼限定。</li>
+
+        </ul>
+      </div>
+
+
+
+
+
+
+</div>
+
+
+
+<h2>メモ用</h2>
+<p>
+id
+username
+nickname
+gender
+age
+comment
+spoiler_comment
+keyword
+is_for_beginner
+post_date
+</p>
+
+<p>時間無かったら投稿フォームを別窓で出そう</p>
+
+<hr>
+
+<h2>投稿フォーム</h2>
+<div class="comment_form">
+  <form action="movie_info/movie_info_create.php" method="post">
+
+  <div>
+    username:
+    <input type="text" name="username">
+    </div>
+
+    <div>
+    nickname:
+    <input type="text" name="nickname">
+    </div>
+
+    <div>
+    gender:
+    <select name="gender" id="gender">
+        <option value="男">男</option>
+        <option value="女">女</option>
+        <option value="どちらでもない">どちらでもない</option>
+   </select>
+   </div>
+
+   <div>
+   age:
+      <select name="age" id="age">
+        <option value="10代前半">10代前半</option>
+        <option value="10代後半">10代後半</option>
+        <option value="20代前半">20代前半</option>
+        <option value="20代後半">20代後半</option>
+      </select>
+   </div>
+
+    <div>
+    comment:(textareaに変更する)
+    <input type="text" name="comment">
+
+    <div>
+    spoiler_comment:
+    <input type="text" name="spoiler_comment">
+    </div>
+
+    <div>
+    keyword:
+    <input type="text" name="keyword">
+    </div>
+
+    <div>
+    is_for_beginner:
+    <input type="text" name="is_for_beginner">
+    </div>
+
+    <div>
+      <button>書き込む</button>
+    </div>
+
+  </form>
+</div>
+
 </body>
 </html>
